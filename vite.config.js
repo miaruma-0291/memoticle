72fb69431c4e48e96edb9f592abac09a7782ed14
miaruma <imamura.iy@gmail.com>
2025-06-01 20:05:44 +0900

    vite.config.js修正_GitHub Pages用

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/memoticle/', // GitHub Pages用 リポジトリのサブディレクトリでホストされるので、Vue アプリ内のリンクや画像などを正しく解決するために base パスの設定が必要
+
   plugins: [
     vue(),
     vueDevTools(),
